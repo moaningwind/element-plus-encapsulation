c3353107ed4499239d1460dc06f5a2f0b35b0935
Wind <zhangyf@phxg.cn>
2023-06-16 15:47:20 +0800

    migrate useCountTo

---
+++ src/composables/useCountTo.ts
@@ -0,0 +1,97 @@
+import type { MaybeRef, UseRafFnCallbackArguments } from '@vueuse/core'
+import { tryOnMounted, useRafFn } from '@vueuse/core'
+import { isRef, ref, unref, watch } from 'vue'
+
+export interface CountToOptions {
+  startVal?: MaybeRef<number>
+  endVal?: MaybeRef<number>
+  duration?: number
+  autoplay?: boolean
+  useEasing?: boolean
+  easingFn?: Function
+}
+
+// https://easings.net/
+function easeInQuad(x: number): number {
+  return x * x
+}
+
+export function useCountTo(options?: CountToOptions) {
+  const {
+    startVal = 0,
+    endVal = 100,
+    duration = 3000,
+    autoplay = true,
+    easingFn = easeInQuad,
+  } = options ?? {}
+
+  let realStartVal: number
+  let realDuration: number
+  let startTime: number
+  let remaining: number
+
+  const num = ref(startVal)
+
+  const { pause: pauseRaf, resume: resumeRaf } = useRafFn(count, { immediate: false })
+
+  tryOnMounted(() => {
+    if (autoplay)
+      start()
+  })
+
+  if (isRef(startVal)) {
+    watch(startVal, () => {
+      if (autoplay)
+        start()
+    })
+  }
+
+  if (isRef(endVal)) {
+    watch(endVal, () => {
+      if (autoplay)
+        start()
+    })
+  }
+
+  function start() {
+    startTime = 0
+    realStartVal = num.value = unref(startVal)
+    realDuration = duration
+    resumeRaf()
+  }
+
+  function pause() {
+    pauseRaf()
+  }
+
+  function resume() {
+    if (remaining > 0) {
+      startTime = 0
+      realStartVal = unref(num)
+      realDuration = remaining
+      resumeRaf()
+    }
+  }
+
+  function count({ timestamp }: UseRafFnCallbackArguments) {
+    if (!startTime)
+      startTime = timestamp
+
+    const passTime = timestamp - startTime
+
+    const t = passTime > realDuration ? 1 : passTime / realDuration
+
+    num.value = realStartVal + (unref(endVal) - realStartVal) * easingFn(t)
+
+    remaining = realDuration - passTime
+    if (remaining < 0)
+      pause()
+  }
+
+  return {
+    num,
+    start,
+    pause,
+    resume,
+  }
+}
--- src/pages/ProgressPage.vue
+++ src/pages/ProgressPage.vue
@@ -1,7 +1,29 @@
+<script setup lang="ts">
+function easeOutExpo(x: number): number {
+  return x === 1 ? 1 : 1 - 2 ** (-10 * x)
+}
+
+const { pause, resume, num } = useCountTo({
+  startVal: 0,
+  endVal: 100,
+  easingFn: easeOutExpo,
+})
+
+const percentage = computed(() => {
+  return +num.value.toFixed(2)
+})
+</script>
+
 <template>
-  <Progress is-animate :percentage="60" />
-  <br>
-  <Progress is-animate :time="5000" status="success" :percentage="60" />
-  <br>
-  <Progress type="circle" :percentage="60" />
+  <el-progress :percentage="percentage" />
+  <el-progress status="success" :percentage="percentage" />
+  <el-progress type="circle" :percentage="percentage" />
+  <div space="x-2">
+    <button btn @click="pause">
+      pause
+    </button>
+    <button btn @click="resume">
+      resume
+    </button>
+  </div>
 </template>
