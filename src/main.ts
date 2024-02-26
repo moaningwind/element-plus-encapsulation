b0cbac145448a8be42cae893d500dbdb1656598a
Wind <zhangyf@phxg.cn>
2024-02-26 10:34:00 +0800

    chore: update

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,8 @@ import 'element-plus/theme-chalk/dark/css-vars.css'
 import App from './App.vue'
 import './styles/main.css'
 
+import.meta.glob('@/store/*.ts', { eager: true })
+
 const app = createApp(App)
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
---
+++ src/store/app.ts
@@ -0,0 +1,5 @@
+const app = useLocalStorage('app', {
+  name: 'playground',
+})
+
+export default app
